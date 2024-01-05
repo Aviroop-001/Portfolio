df0b902ed881061a09586c4805371ff6ab58da25
Aviroop-001 <banerjeeaviroop01@gmail.com>
2024-01-05 12:19:24 +0530

    Added speed insight scripts

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
+    <SpeedInsights />
     <Analytics />
   </React.StrictMode>,
   document.getElementById("root")
